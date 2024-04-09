480eec01bc578033609b193b6431994ba44c2e6a
Ank <ank215900@gmail.com>
2024-04-09 16:34:34 +0300

    Complete homework lesson 4

---
+++ Homework_lesson_4/task_1.py
@@ -0,0 +1,98 @@
+# Aceasta este prima ta sarcină a lecției legată de float în python
+
+# Creează o variabilă numită `age` și seteaz-o la vârsta ta
+
+# CODUL TĂU VINE MAI JOS:
+age = 36
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tipul variabilei `age` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(type(age))
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum asigură-te că variabila `age` este de tipul float.
+
+# CODUL TĂU VINE MAI JOS:
+age = float(age)
+print(type(age))
+
+# CODUL TĂU VINE MAI SUS:
+
+# Creează două variabile numite `age2` și `age3` și setează-le la vârsta prietenilor tăi, 
+# ambele variabile trebuie să fie de tipul float.
+
+# CODUL TĂU VINE MAI JOS:
+age2 = 26
+age3 = 37
+age2 = float(age2)
+age3 = float(age3)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează suma vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+age_sum = age + age2 + age3
+print(age_sum)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează diferența vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+age_dif1 = age - age2
+age_dif2 = age - age3
+print(age_dif1)
+print(age_dif2)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează produsul vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+age_multiply1 = age * age2 * age3
+print(age_multiply1)
+print(age * age2)
+print(age * age3)
+print(age2 * age3)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează împărțirea vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+divided1 = age / age2
+divided2 = age / age3
+print(divided1)
+print(divided2)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează restul împărțirii vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(divided1 % 10)
+print(divided2 % 10)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează rezultatul vârstei tale ridicată la puterea vârstei primului prieten folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age ** age2)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează rezultatul vârstei primului prieten împărțită la puterea vârstei celui de-al doilea prieten 
+# folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2 ** age3)
+
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
